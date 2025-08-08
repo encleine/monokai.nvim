2a02044250988d9ffb9944ca2be99f795aab7390
encleine <encleine@gmail.com>
2025-08-09 02:23:47 +0300

    revert last commit

--- lua/monokai/init.lua
+++ lua/monokai/init.lua
@@ -613,11 +613,7 @@ M.setup = function(config)
 	config = config or {}
 	config = vim.tbl_deep_extend('keep', config, default_config)
 
-	local used_palette = M[config.palette]
-	if not used_palette then
-		used_palette = M.classic
-	end
-
+	local used_palette = config.palette or M.classic
 	vim.g.colors_name = used_palette.name
 
 	local syntax = M.load_syntax(used_palette)
